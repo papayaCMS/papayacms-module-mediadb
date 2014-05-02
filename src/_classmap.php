c4068349ce522c01ff393fb27ad3d5c180d2afb2
Thomas Weinert <weinert@dimensional.de>
2014-05-02 15:18:51 +0200

    # Initial Commit, separate module from cms/framework repository

---
+++ src/_classmap.php
@@ -0,0 +1,7 @@
+<?php
+return array(
+  'actionbox_thumbnail' => '/actbox_thumbnail.php',
+  'content_download' => '/content_download.php',
+  'content_thumbs' => '/content_thumbs.php',
+  'content_upload' => '/content_upload.php',
+);
